eb7eb2c610fe0dd665cfa65ce6a36257bf7a592c
Taylor faye <taylorpierson48@yahoo.com>
2018-08-02 18:44:31 -0400

    add more methods

---
+++ TextDisplayer/Food.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDisplayer
+{
+    class Food
+    {
+        private string Name;
+        private string Color;
+        private int timeCooked;
+        public static int numberOfFoodGroups = 5;
+        public static int NumOfFoodGroups()
+        {
+            return numberOfFoodGroups;
+        }
+        public string NameOfFood()
+        {
+              return this.Name;
+        }
+
+        public string ColorOfFood()
+        {
+            return this.Color;
+        }
+
+        private void AddTimeCooked(int seconds)
+        {
+            this.timeCooked += seconds;
+            
+        }
+
+        public void Cook(int seconds)
+        {
+            this.AddTimeCooked(seconds);
+        }
+
+        public int GetTimeCooked()
+        {
+            return this.timeCooked;
+        }
+
+        public Food()
+        {
+            this.timeCooked = 0;
+        }
+
+        public Food(string name, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+            this.timeCooked = 0;
+        }
+
+
+    }
+}
---
+++ TextDisplayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDisplayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cheese = new Food("gouda", "blue");
+            
+            var notCheese = new Food("steaaakkk", "not blue");
+
+            var fruit = new Food("apple", "red");
+
+            var veggie = new Food("potato", "white");
+
+            notCheese.Cook(40);
+            veggie.Cook(25);
+
+
+
+
+            var foodList = new List<Food>();
+            foodList.Add(cheese);
+            foodList.Add(notCheese);
+            foodList.Add(fruit);
+            foodList.Add(veggie);
+
+            foreach(var foodItem in foodList)
+            {
+                Console.WriteLine(foodItem.NameOfFood());
+                Console.WriteLine(foodItem.ColorOfFood());
+                Console.WriteLine(foodItem.GetTimeCooked());
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
